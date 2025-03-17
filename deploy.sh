2e0dbd09242842093588dc341e601f5e661f412b
NamanUM <160727708+NamanUM@users.noreply.github.com>
2025-03-17 18:33:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 # This is to demo
 
-node_app=`docker ps -a | grep imag'`
-if [ $node_app=='imag' ]; then
+node_app=`docker ps -a | grep image'`
+if [ $node_app=='image' ]; then
     echo "nodeapp is running, lets delete"
         docker rm -f nodeapp
 fi
 
-images=`docker images | grep good777lord/imag '`
+images=`docker images | grep naman211/image '`
 docker rmi $images

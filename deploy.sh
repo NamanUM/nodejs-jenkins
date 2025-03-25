7f7d8555a8850d539687cdfa6f3dfbc6c87d8fca
NamanUM <160727708+NamanUM@users.noreply.github.com>
2025-03-25 19:01:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-# This is to demo
+NEW_IMAGE_NAME="naman211/my-new-image:latest" # Change this to your new image name
 
-node_app=$(docker ps -a | grep 'image')
+# Remove the container based on the name of the image it uses.
+node_app=$(docker ps -a --filter "ancestor=$NEW_IMAGE_NAME" | awk 'NR>1 {print $1}')
 
 if [[ -n "$node_app" ]]; then
-    echo "nodeapp is running, lets delete"
-        docker rm -f $(docker ps -a | grep 'image' | awk '{print $1}')
+    echo "$NEW_IMAGE_NAME container is running, lets delete"
+    docker rm -f $node_app
 fi
 
-images=$(docker images | grep 'naman211/image' | awk '{print $3}')
+# Remove the old image itself.
+images=$(docker images | grep "$NEW_IMAGE_NAME" | awk '{print $3}')
 
 if [[ -n "$images" ]]; then
     docker rmi $images
 fi
+
+# Pull the new image from Docker Hub.
+docker pull $NEW_IMAGE_NAME
+
+# Run a new container using the pulled image.
+docker run -d -p 3500:3500 --name my-new-container $NEW_IMAGE_NAME
